57e15bd0def32bc9c78937964becb831cbd0ac74
Raunak Kumar <kumarraunak085@gmail.com>
2025-09-17 05:27:47 +0530

    ready for deploy

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,4 +1,33 @@
-// server.js
+// // server.js
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import router from "../route.js";
+
+// dotenv.config();
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Routes
+// app.use(router);
+
+// // Default route
+// app.get("/", (req, res) => {
+//   res.send("Backend is running 🚀");
+// });
+
+// // Start server
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => {
+//   console.log(`✅ Server running on http://localhost:${PORT}`);
+// });
+
+///// serverless function version
+// backend/src/api/server.js
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -20,8 +49,5 @@ app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
-// Start server
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+
+export default app;
